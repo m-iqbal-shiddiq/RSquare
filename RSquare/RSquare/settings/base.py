0071a55b2428ac2a29384d64faec9c5c7c1e58d4
g64160023 <auliapadli12@yahoo.com>
2019-11-24 16:44:05 +0700

    fix admin page error tiba tiba (gjls bgsd)

--- RSquare/RSquare/settings/base.py
+++ RSquare/RSquare/settings/base.py
@@ -165,3 +165,4 @@ WAGTAIL_SITE_NAME = "RSquare"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+SITE_ID=1
