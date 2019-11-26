1f114b0cf5c9d4584ddad8bacb701a290ca5b138
g64160023 <auliapadli12@yahoo.com>
2019-11-26 21:00:43 +0700

    fungsi delete pesanan kelar

--- RSquare/ListPesanan/urls.py
+++ RSquare/ListPesanan/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ListPesananView
-
+app_name = 'listpesanan'
 urlpatterns = [
-   path('', ListPesananView.as_view(template_name = "ListPesanan/listpesanan.html"), name='contact')
+   path('', ListPesananView.as_view(template_name = "ListPesanan/listpesanan.html"), name='contact'),
+   path('<pk>', ListPesananView.hapuspesanan, name='deletepesanan')
 ]
--- RSquare/ListPesanan/views.py
+++ RSquare/ListPesanan/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from formpesanan.forms import FormPesanan
 from formpesanan.models import Pesanan
+from django.http import HttpResponseRedirect
 # Create your views here.
 class ListPesananView(TemplateView):
     template_name = 'listpesanan.html'
@@ -11,3 +12,8 @@ class ListPesananView(TemplateView):
         args = {'pesanans':pesanan}
         print(pesanan)
         return render(request, self.template_name, args)
+
+    def hapuspesanan(self,pk):
+        pesanan = Pesanan.objects.get(id=pk)
+        pesanan.delete()
+        return HttpResponseRedirect("/listpesanan/")
