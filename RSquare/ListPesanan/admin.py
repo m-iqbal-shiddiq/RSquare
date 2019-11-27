8936053b5eba17475779b9c651c2b98a62caa576
g64160023 <auliapadli12@yahoo.com>
2019-11-27 19:24:30 +0700

    hilangin create sama edit di listpesanan admin

--- RSquare/ListPesanan/admin.py
+++ RSquare/ListPesanan/admin.py
@@ -2,11 +2,21 @@ from django.contrib import admin
 from formpesanan.models import Pesanan
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, modeladmin_register)
+from wagtail.contrib.modeladmin.helpers import PermissionHelper
 # Register your models here.
+
+class ValidationPermissionHelper(PermissionHelper):
+    def user_can_list(self, user):
+        return True
+    def user_can_create(self, user):
+        return False
+    def user_can_edit_obj(self, user, obj):
+        return False
 class ListPesananAdmin(ModelAdmin):
 
     model = Pesanan
     menu_label = "List Pesanan"
+    permission_helper_class = ValidationPermissionHelper
     menu_icon = "placeholder"
     menu_order = 290
     add_to_settings_menu = False
